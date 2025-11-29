86992922887b1b77fec1e163cd84b0fe96a79682
Alex Souza <alexjoseps@icloud.com>
2025-11-28 21:41:10 -0300

    chore: constraints, acoes e navegacao

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        print("Botão iniciar pressionado!")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setBackgroundColor()
     }
 
+    func setBackgroundColor() {
+        startButton.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+    }
 
 }
 
